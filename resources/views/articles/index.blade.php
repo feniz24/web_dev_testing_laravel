f568ad21729384382037ab13c4ccc36c7ff69dec
Feniz <feniztho@gmail.com>
2022-12-01 09:03:55 +0630

    laravel blog

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,15 +1,24 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="UTF-8">
-    <title>Article List</title>
-</head>
-<body>
-    <h1>Article List</h1>
-    <ul>
-        @foreach ($articles as $article)
-            <li>{{ $article['title'] }}</li>
+@extends("layouts.app")
+
+@section("content")
+    <div class="container">
+
+        {{ $articles->links() }}
+
+        @foreach($articles as $article)
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $article->title }}</h5>
+                    <div class="card-subtitle mb-2 text-muted small">
+                        {{ $article->created_at->diffForHumans() }}
+                    </div>
+                    <p class="card-text">{{ $article->body }}</p>
+                    <a class="card-link"
+                        href="{{ url("/>articles/detail/$article->id") }} ">
+                        View Detail &raquo;
+                    </a>
+                </div>
+            </div>
         @endforeach
-    </ul>
-</body>
-</html>
+    </div>
+@endsection
